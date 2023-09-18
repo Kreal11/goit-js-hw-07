64329bf20d5cc69a4eb7ff29e42b388c823c42da
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-18 17:17:38 +0300

    first try

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+let newListArray = [];
+
+const createList = galleryItems.forEach(itemOfGallery => {
+    const item = `<li class="gallery__item">
+  <a class="gallery__link" href="${itemOfGallery.original}">
+    <img
+      class="gallery__image"
+      src="${itemOfGallery.preview}"
+      data-source="large-image.jpg"
+      alt="${itemOfGallery.description}"
+    />
+  </a>
+</li>`
+    newListArray.push(item);
+});
+newListArray.join('');
+
+list.innerHTML = newListArray;
+
+link.addEventListener('click', )
