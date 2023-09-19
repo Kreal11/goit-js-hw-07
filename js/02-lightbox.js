e0160ac1fb5bd5190f3f8b84f482e2f2a94d7bca
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-19 13:21:07 +0300

    update 2 task hw-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -24,7 +24,7 @@ list.insertAdjacentHTML('afterbegin', galleryImages);
 
 let lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
-    captionPosition: 'bottom',
+    // captionPosition: 'bottom',
     captionDelay: 250,
 });
 
